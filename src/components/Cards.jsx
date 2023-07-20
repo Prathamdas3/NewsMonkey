d7bb4f71079de4e656c8e6b8ec51b389ea5154d5
Prathamdas3 <116100433+Prathamdas3@users.noreply.github.com>
2023-07-20 14:44:59 +0530

    feat: vertical Card creation complete

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,7 +1,25 @@
 import React from "react";
-
+import Test from "../assets/test.jpg";
 const Cards = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="flex flex-col rounded-xl bg-slate-300 w-72 m-3 p-4">
+        <div className="">
+          <img src={Test} alt="" className="h-64 w-72 rounded-t-xl " />
+        </div>
+        <div>
+          <h5 className="text-3xl font-medium">title</h5>
+          <p className="text-lg ">description</p>
+          <p>
+            <small className="text-slate-500">date</small>
+          </p>
+          <button type="button" className="rounded-[10px] p-2 bg-blue-500 ">
+            Read Me
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Cards;
